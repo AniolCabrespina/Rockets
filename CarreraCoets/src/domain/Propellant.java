8c85ac4dd3664dde7f7fee8fa92f168ad85e68ed
Aniol Cabrespina <acabrespina@edu.tecnocampus.cat>
2020-05-05 14:14:28 +0200

    Propellant Implemented

--- CarreraCoets/src/domain/Propellant.java
+++ CarreraCoets/src/domain/Propellant.java
@@ -1,5 +1,28 @@
 package domain;
 
 public class Propellant {
+	
+	private float maximumAcceleration;
+	private float currentAcceleration;
+	
+	public Propellant (float maximumAcceleration) {
+		this.maximumAcceleration = maximumAcceleration;
+		this.currentAcceleration = 0.0f;
+	}
+	
+	public float getMaximumAcceleration() {
+		return maximumAcceleration;
+	}
+	
+	public float getCurrentAcceleration() {
+		return currentAcceleration;
+	}
+	
+	public void increaseCurrentAcceleration(float increase) throws Exception {
+		if(currentAcceleration + increase > maximumAcceleration) {
+			this.currentAcceleration = this.maximumAcceleration;
+		}
+		this.currentAcceleration += increase;
+	}
 
 }
