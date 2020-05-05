6c5742076422a2ab49741def36fcb775386d7a31
Adrià Pujol <apujols@edu.tecnocampus.cat>
2020-05-05 14:39:18 +0200

    Rocket i Deposit implementats

--- CarreraCoets/src/domain/Deposit.java
+++ CarreraCoets/src/domain/Deposit.java
@@ -2,4 +2,24 @@ package domain;
 
 public class Deposit {
 
+	private float totalFuel;
+	private float currentFuel;
+	private float consumption;
+	
+	public Deposit(float total) {
+		this.totalFuel = total;
+		this.currentFuel = this.totalFuel;
+	}
+	
+	public float getTotalValue() {return totalFuel;}
+	public float getCurrentValue() {return currentFuel;}
+	public float getConsumption() {return consumption;}
+	
+	public void calculateConsumption(float velocity) {		
+		consumption = (float) (0.02f * Math.pow(velocity, 2));
+	}
+	
+	public void recalculateValue() {
+		currentFuel -= consumption;
+	}
 }
--- CarreraCoets/src/domain/Rocket.java
+++ CarreraCoets/src/domain/Rocket.java
@@ -1,5 +1,53 @@
 package domain;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Rocket {
+
+	private List<Propellant> propellants;
+	private Deposit deposit;
+	private float currentAcceleration;
+	private float currentVelocity;
+	private float currentMeters;
+
+	public Rocket(Deposit deposit) {
+		this.deposit = deposit;
+		this.propellants = new LinkedList<Propellant>();
+		this.currentVelocity = 0.0f;
+		this.currentMeters = 0.0f;
+	}
+
+	public float getVelocity() {
+		return currentVelocity;
+	}
+
+	public float getMeters() {
+		return currentMeters;
+	}
+
+	public void updateAcceleration() {
+		for(int i = 0; i < propellants.size(); i++) {
+			currentAcceleration += propellants.get(i).getCurrentAcceleration();
+		}
+	}
+
+	public void updateVelocity(float time) {
+		
+		this.currentVelocity = this.currentVelocity + currentAcceleration * time; 
+		
+	}
+
+	public void updateMeters(float time) {
+		
+		this.currentMeters = (float) (this.currentMeters
+				+ (this.currentVelocity * time) + ((currentAcceleration/2) * Math.pow(time,2)));
+	}
+
+	public void addRocketPropellants(List<Propellant> propellants) {
+		for (int i = 0; i < propellants.size(); i++) {
+			this.propellants.add(propellants.get(i));
+		}
+	}
 
 }
